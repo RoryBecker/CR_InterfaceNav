a4007fdda64b35668c12803e0259da8ad1d2a5e7
Rory Becker <RoryBecker@gmail.com>
2014-12-08 23:19:26 +0000

    Initial Load

---
+++ CR_InterfaceNav/CR_InterfaceNavExtension.vb
@@ -0,0 +1,7 @@
+Imports System.ComponentModel.Composition
+Imports DevExpress.CodeRush.Common
+
+<Export(GetType(IVsixPluginExtension))> _
+Public Class DX_InterfaceNavExtension
+    Implements IVsixPluginExtension
+End Class
